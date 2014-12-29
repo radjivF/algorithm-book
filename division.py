4c14d84a6e15fbf470579aa482a48cb4ec66b872
radjivC <carrereradjiv@yahoo.fr>
2014-12-29 02:10:48 +0100

    add divide with recursivity

---
+++ division.py
@@ -0,0 +1,17 @@
+def divide(x, y):
+    if x == 0: return (0, 0)
+    quotient, rest = divide(x/2, y)
+    quotient *= 2
+    rest *= 2
+    if x % 2 == 1:
+        rest += 1
+    if rest >= y:
+        rest -= y
+        quotient += 1
+    print "quotient ", quotient,"rest ", rest
+    return (quotient, rest)
+
+#assert divide(4, 2) == (2, 0)
+assert divide(17, 3) == (5, 2)
+# assert divide(10, 15) == (0, 10)
+# assert divide(14, 5) == (2, 4)
