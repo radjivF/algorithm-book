30c95e64658214f6a3e6ee0abcc9106fc15b9409
radjivC <carrereradjiv@yahoo.fr>
2015-01-02 16:31:05 +0100

    greddy knapsack problem

--- knapsack.rb
+++ knapsack.rb
@@ -1,15 +1,19 @@
-def knapSack(totalWeight, objects):
-2    currentWeight = 0
-3    subSet = []
-4    for counter in range(len(objects):
-5        if currentWeight + objects[counter][-1] <= totalWeight:
-6            currentWeight += objects[counter][-1]
-7            subSet.append(objects[counter])
-8        return subSet
-9
-10def retrivevingData():
-11    objects = input("Veuillez entrer le set d'objets sous la forme \"liste de liste\" (e.g. [[objet1, valeur1, poids1],[objet2,valeur2,poids2]]) par ordre décroissant de valeur :")
-12    totalWeight = input("Veuillez entrer la capacité maximale de votre sac à dos :")
-13    return knapSack(totalWeight, objects):
-14
-15print "La proposition de la méthode gloutonne est :", retrievingData()
+def greedyMethod(moneySystem, giveBack, solution):
+  if giveBack == 0:
+    return solution
+    while giveBack >= moneySystem[0]:
+      giveBack -= moneySystem[0]
+      solution.append(moneySystem[0])
+    else:
+      return greedyMethod(moneySystem[1:len(moneySystem)], giveBack, solution)
+
+def retrievingData():
+  moneySystem = input("Entrez le système monétaire de façon décroissante sous forme de liste (e.g. [200,100,50,20,10,5,2,1] pour le système européen)")
+  giveBack = input("Entrez la somme à rendre")
+  solution = []
+  if moneySystem == [] or giveBack <= 0:
+    print "Vous vous êtes trompé lors de l'encodage des données"
+  else:
+    return greedyMethod(moneySystem, giveBack, solution)
+
+    print "Le choix proposé par la méthode gloutonne est :", retrievingData()
