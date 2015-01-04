94fcb73f47d9c16eae685bc091d34b9593ebde88
radjivC <carrereradjiv@yahoo.fr>
2015-01-04 22:07:20 +0100

    breath first search

--- graph/breathFirstSearch/bfsShortestPath.py
+++ graph/breathFirstSearch/bfsShortestPath.py
@@ -5,11 +5,22 @@ graph = {'A': set(['B', 'C']),
          'E': set(['B', 'F']),
          'F': set(['C', 'E'])}
 
+def bfs_paths(graph, start, goal):
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next] #yield = return because its a generator and dont stop the program
+            else:
+                queue.append((next, path + [next]))
+
+
+
 # with the bfs you can have access to this information
 def shortest_path(graph, start, goal):
     try:
         return next(bfs_paths(graph, start, goal))
     except StopIteration:
         return None
-
 shortest_path(graph, 'A', 'F') # ['A', 'C', 'F']
