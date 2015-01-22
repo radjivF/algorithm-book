d07ab9124576470bb0b82eaf2c52794a81a51ad6
radjiv fender <carrereradjiv@yahoo.fr>
2015-01-22 12:30:40 +0100

    deleted last folder

--- binaryThree/binaryTree.py
+++
@@ -1,7 +0,0 @@
-class Tree(object):
-    def __init__(self, entry, left=None, right=None):
-        self.entry = entry
-        self.left = left
-        self.right = right
-
-t = Tree(5, Tree(1, None, Tree(4)), Tree(7, Tree(6), Tree(8)))
